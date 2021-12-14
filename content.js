c19d5581cf61b80ad975a4cdcf0c195ed92f0025
ARAVIND CHANDRASHEKAR REDDY <achandrashekarr@deakin.edu.au>
2021-12-14 11:56:46 +0530

    Changed the main content and added description

--- content.js
+++ content.js
@@ -1,32 +1,11 @@
-// $("p").mouseover(function(){
-//     $(this).css("background-color", "blue");
-// })
-// $("p").mouseout(function(){
-//     $(this).css("background-color", "");
-// })
-// $('p').html(function() {
-//     debugger;
-//     var content = $("p:first").text();
-    
-
-//     return $(this).text().replace(/\w+[,.]?\s+?/g , "<span>$&</span>")
-// });
-
-// // // $('p').on('mouseenter', 'span', function() {
-// // //     var $span = $(this);
-// // //     $('p span').filter(function() {
-// // //         return $(this).offset().top == $span.offset().top;
-// // //     }).css("background-color", "yellow");
-// // // }).on('mouseleave', 'span', function() {
-// // //     $('p span').css("background-color", "");
-// // // })     
+  
 $("p").each(function(){
     var text = $(this).text();
     if(text == ""){
     }
     else{
         var p = "" ; 
-        var FullStops = text.split(".");
+        var FullStops = text.split(". ");
         FullStops.forEach(Element => {
             p  = p + "<span>" + Element +  "." + "</span>";
         });
@@ -41,15 +20,4 @@ $("span").mouseover(function(){
 $("span").mouseout(function(){
     $(this).css("background-color", "");
 })
-
-
-// $('p').on('mouseenter', 'span', function() {
-//     var $span = $(this);
-//     $('p span').filter(function() {
-//         return $(this).offset().top == $span.offset().top;
-//     }).css("background-color", "skyblue");
-// }).on('mouseleave', 'span', function() {
-//     $('p span').css("background-color", "");
-// })  
-
 
